f32b1d708f6bcba477100ed250aa6468349d76cf
Dimon Stelmakh <105310426+DimonStelmakh@users.noreply.github.com>
2024-12-27 04:22:21 +0200

    some .gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,5 +73,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("at.favre.lib:bcrypt:0.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 //    implementation("androidx.compose.ui:ui-text-google-fonts:$compose_version")
 }
